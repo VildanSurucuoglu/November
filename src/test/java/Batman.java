63be379ac2737bf4b089aed6a4d924e19d94d0d6
VildanSurucuoglu <vildanorhan10@gmail.com>
2020-11-01 10:53:48 -0500

    Batman says

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -3,6 +3,7 @@ public class Batman {
     public static void main(String[] args) {
 
         System.out.println("I will catch you Joker");
+        System.out.println("I will catch you Joker");
 
 
     }
